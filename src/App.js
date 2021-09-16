fb9567fe6deee1f3811fa969c09466390f722f48
jesus8850 <estebanguerrieri8850@gmail.com>
2021-09-15 22:48:15 -0300

    nuevo

--- src/App.js
+++ src/App.js
@@ -5,20 +5,7 @@ import NavBar from './components/NavBar/NavBar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
       <NavBar/>
-        <img src={logo} className="App-logo" alt="logo" />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
   );
 }
 
