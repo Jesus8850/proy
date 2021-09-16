0d5dbdfc2239a19e4e7b4469b21800a62237fae8
jesus8850 <estebanguerrieri8850@gmail.com>
2021-09-15 22:35:05 -0300

    cambios

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/NavBar/NavBar';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <NavBar/>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          HOLA MUNDO
-        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
